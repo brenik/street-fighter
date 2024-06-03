ef66e02c0ad45d94bc96fee5b28a4add92b91630
brenik <vitaliy_brenik@outlook.com>
2024-06-04 00:20:44 +0300

    clear

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const API_URL = 'https://api.github.com/repos/brenik/street-fighter/contents/resources/api/fighters.json';
 const SECURITY_HEADERS = {
     headers: {
-        authorization: "token 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"
+        authorization: "token %you token%"
     }
 };
 
